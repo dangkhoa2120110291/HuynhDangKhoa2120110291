d9f1fc2ac76f84f573543a4ac454d6e537531481
QuanLyBanHang <huynhdangkhoa080102@gmail.com>
2023-09-19 20:11:32 +0700

    admin

--- WebsiteBanHang/App_Start/RouteConfig.cs
+++ WebsiteBanHang/App_Start/RouteConfig.cs
@@ -14,10 +14,11 @@ namespace WebsiteBanHang
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+    "Default",
+    "{controller}/{action}/{id}",
+    new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+    new[] { "WebsiteBanHang.Controllers" }
+);
         }
     }
 }
